7b4e024525240c3348469bb4684f5d3e07c16ce2
AllenKeelig <allen.keeling@atlasschool.com>
2025-03-19 12:43:04 -0500

    6

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema'); 
-
+const mongoose = require('mongoose');
 const app = express();
+const Task = require('../models/task');
+const Project = require('../models/project');
+
+mongoose
+  .connect('mongodb+srv://allenkeeling:MXbnmtdVM23sKK2M@cluster0.2kxxf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch(err => console.error('MongoDB connection error:', err));
+  mongoose.connection.once(’open’, () =>
+  console.log(’connected to database’);
+);
 
 app.use('/graphql', graphqlHTTP({
   schema,
